7a6f4cae1d23b398a8f381ac32d6be2fe046c5b7
Curtis Delicata <curtis@liberugroup.com>
2026-08-29 18:03:21 +0100

    Synchronize maintenance module (#3)
    Co-authored-by: curtisdelicata <bot@liberu.software>

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,7 @@ Route::middleware('auth:sanctum')->prefix('api/v1/maintenance/labor-and-time')->
     Route::get('/', [TimeEntryController::class, 'index']);
     Route::post('/', [TimeEntryController::class, 'store']);
     Route::get('/{timeEntry}', [TimeEntryController::class, 'show']);
+    Route::patch('/{timeEntry}', [TimeEntryController::class, 'update']);
+    Route::delete('/{timeEntry}', [TimeEntryController::class, 'destroy']);
     Route::post('/{timeEntry}/approve', [TimeEntryController::class, 'approve']);
 });
