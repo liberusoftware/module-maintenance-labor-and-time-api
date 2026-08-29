546fc9ada476ff4ab459f547ae6166b55c19560d
Curtis Delicata <curtis@liberugroup.com>
2026-08-29 17:01:00 +0100

    Sync maintenance module implementation
    Merge verified maintenance module synchronization.

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Liberu\Modules\Maintenance\LaborAndTime\Api\Http\Controllers\TimeEntryController;
+
+Route::middleware('auth:sanctum')->prefix('api/v1/maintenance/labor-and-time')->group(function (): void {
+    Route::get('/', [TimeEntryController::class, 'index']);
+    Route::post('/', [TimeEntryController::class, 'store']);
+    Route::get('/{timeEntry}', [TimeEntryController::class, 'show']);
+    Route::post('/{timeEntry}/approve', [TimeEntryController::class, 'approve']);
+});
